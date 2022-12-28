ca832c484213d97b1689c375545844ec91907648
githubchief <ranganathanshams@gmail.com>
2022-12-28 11:54:35 +0530

    create Book constructor and test it

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Files works")
+let myLibrary = [];
+
+function Book(name, author, status) {
+  // the constructor...
+
+  this.name=name;
+  this.author=author;
+  this.status=status;
+
+}
+
+function addBookToLibrary() {
+  // do stuff here
+
+
+}
+
+let favBook=new Book("Sapiens","Harai Yuval", "read");
+
+console.log(favBook.name);
